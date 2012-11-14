022834e4d618eb254814a8d650df2ec8bfa6d42f
jefk <speedracer111@gmail.com>
2012-11-13 21:25:29 -0800

    better style?

--- app.rb
+++ app.rb
@@ -18,17 +18,16 @@ get '/flip/:id' do |id|
 end
 
 post '/flip' do
-  seconds =
-    if params[:seconds_til_flip]
-      halt 400, ":seconds_til_flip cannot be empty" if params[:seconds_til_flip].strip.empty?
-      params[:seconds_til_flip].to_i
-    elsif params[:minutes_til_flip]
-      halt 400, ":minutes_til_flip cannot be empty" if params[:minutes_til_flip].strip.empty?
-      # max time is 59:59
-      [ params[:minutes_til_flip].to_i * 60, 59*60 + 59 ].min
-    else
-      halt 400, "params must include :seconds_til_flip or :minutes_til_flip"
-    end
+  if params[:seconds_til_flip]
+    halt 400, ":seconds_til_flip cannot be empty" if params[:seconds_til_flip].strip.empty?
+    seconds = params[:seconds_til_flip].to_i
+  elsif params[:minutes_til_flip]
+    halt 400, ":minutes_til_flip cannot be empty" if params[:minutes_til_flip].strip.empty?
+    # max time is 59:59
+    seconds = [ params[:minutes_til_flip].to_i * 60, 59*60 + 59 ].min
+  else
+    halt 400, "params must include :seconds_til_flip or :minutes_til_flip"
+  end
 
   id = Flip.create(seconds)
 
